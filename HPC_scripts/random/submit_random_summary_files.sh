64b7102fa5ad00372b9287df1c89c39767213cee
Lisa McManus <lisa.c.mcmanus@gmail.com>
2021-03-17 18:29:29 -1000

    HPC scripts to generate summary files

---
+++ HPC_scripts/random/submit_random_summary_files.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+#SBATCH -N 1
+#SBATCH -c 1
+#SBATCH -t 1:00:00
+#SBATCH --mem=50000 ## memory allotted for the job in MB
+
+# This is to get e-mail notifications
+# when the jobs start and end
+#SBATCH --mail-type=begin
+#SBATCH --mail-type=end
+#SBATCH --mail-user=
+
+# Name of the job as it appears in squeue
+#SBATCH --job-name=rand_summary
+
+#! DELLA
+# Load python
+module load python 
+module load anaconda
+
+# The first argument is the total number of cores you
+# want but we get it from a SLURM environment variable
+python random_summary_files.py
---
+++ HPC_scripts/regular/regular_summary_files_TS.py
@@ -0,0 +1,49 @@
+import numpy as np
+import sys
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+
+###
+numFiles = 20
+
+N_ts_0 = np.zeros((20,3,1500,15,15,numFiles))
+Z_ts_0 = np.zeros((20,3,1500,15,15,numFiles))
+
+for i in np.arange(0,numFiles):
+    N = np.load("N_mat_self_rec0_"+str(i)+".npy")
+    Z = np.load("Z_mat_self_rec0_"+str(i)+".npy")
+    
+    N_ts_0[:,:,:,:,:,i]=N[:,:,:,:,:]
+    Z_ts_0[:,:,:,:,:,i]=Z[:,:,:,:,:]
+    
+N_batch_ave_ts_0 = N_ts_0.mean(axis=5)
+N_batch_std_ts_0 = N_ts_0.std(axis=5)
+Z_batch_ave_ts_0 = Z_ts_0.mean(axis=5)
+Z_batch_std_ts_0 = Z_ts_0.std(axis=5)
+
+np.save("./output/N_reg_ave_ts_0.npy", N_batch_ave_ts_0)
+np.save("./output/N_reg_std_ts_0.npy", N_batch_std_ts_0)
+np.save("./output/Z_reg_ave_ts_0.npy", Z_batch_ave_ts_0)
+np.save("./output/Z_reg_std_ts_0.npy", Z_batch_std_ts_0)
+
+###
+N_ts_1 = np.zeros((20,3,500,15,15,numFiles))
+Z_ts_1 = np.zeros((20,3,500,15,15,numFiles))
+
+for i in np.arange(0,numFiles):
+    N = np.load("N_mat_self_rec_"+str(i)+".npy")
+    Z = np.load("Z_mat_self_rec_"+str(i)+".npy")
+    
+    N_ts_1[:,:,:,:,:,i]=N[:,:,:,:,:]
+    Z_ts_1[:,:,:,:,:,i]=Z[:,:,:,:,:]
+    
+N_batch_ave_ts_1 = N_ts_1.mean(axis=5)
+N_batch_std_ts_1 = N_ts_1.std(axis=5)
+Z_batch_ave_ts_1 = Z_ts_1.mean(axis=5)
+Z_batch_std_ts_1 = Z_ts_1.std(axis=5)
+
+np.save("./output/N_reg_ave_ts_1.npy", N_batch_ave_ts_1)
+np.save("./output/N_reg_std_ts_1.npy", N_batch_std_ts_1)
+np.save("./output/Z_reg_ave_ts_1.npy", Z_batch_ave_ts_1)
+np.save("./output/Z_reg_std_ts_1.npy", Z_batch_std_ts_1)
